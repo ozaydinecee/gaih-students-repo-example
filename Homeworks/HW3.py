ebb7c1a19dea2f1f113d18e9f8cdc2e7d06e8b35
ozaydinecee <77552205+ozaydinecee@users.noreply.github.com>
2021-03-14 21:02:23 +0300

    Update HW3.py
    OUTPUT:
    Average marks of Ece is :69.9 
    Ece 's grades: 
     project grade: 80 
     midterm grade: 75 
     final grade: 78
    Average marks of Ali is :68.69999999999999 
    Ali 's grades: 
     project grade: 82 
     midterm grade: 80 
     final grade: 67
    Average marks of Ege is :70.5 
    Ege 's grades: 
     project grade: 77 
     midterm grade: 78 
     final grade: 80
    Average marks of Efe is :52.8 
    Efe 's grades: 
     project grade: 67 
     midterm grade: 40 
     final grade: 69
    Average marks of Eda is :43.2 
    Eda 's grades: 
     project grade: 29 
     midterm grade: 65 
     final grade: 50

--- Homeworks/HW3.py
+++ Homeworks/HW3.py
@@ -1,5 +1,22 @@
-#Explain your work
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 14 20:25:56 2021
 
-#Question 1
-for x in range(a):
-	print(a)
+@author: Ece Ozaydin
+@date 14.03.2021
+"""
+ece = { "name":"Ece","project" : 80,"midterm" : 75,"final" : 78} 
+ali = { "name":"Ali","project" : 82,"midterm" : 80,"final" : 67} 
+ege = { "name":"Ege","project" : 77,"midterm" : 78,"final" : 80} 
+efe = { "name":"Efe","project" : 67,"midterm" : 40,"final" : 69} 
+eda = { "name":"Eda","project" : 29,"midterm" : 65,"final" : 50} 
+
+def calculate_passing_grade(students):
+    passing_grade=students["project"]*(0.3)+students["midterm"]*(0.3)+students["final"]*(0.3)
+    return passing_grade
+
+students=[ece,ali,ege,efe,eda]
+
+for i in students :
+    print("Average marks of {0} is :{1} " .format(i["name"], calculate_passing_grade(i))) 
+    print("{0} 's grades: \n project grade: {1} \n midterm grade: {2} \n final grade: {3}".format(i["name"],i["project"],i["midterm"],i["final"]))
